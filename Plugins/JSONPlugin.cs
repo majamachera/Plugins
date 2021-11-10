8a4b6ba8100f183199690a158063ebd5a2eb3a49
maja.machera <maja.machera@KRK1-LHP-P00885>
2021-11-10 03:37:10 +0100

    Add project files.

---
+++ Plugins/JSONPlugin.cs
@@ -0,0 +1,40 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Task_03_11;
+
+namespace Plugins
+{
+    public class JSONPlugin : ISerializePlugin
+    {
+       
+        public string Name => "JSON";
+
+        public string Description => "JSON is an open standard file format and data interchange format that uses human-readable text\r\n to store and transmit data objects consisting of attribute–value pairs and arrays. \r\n It is a common data format with diverse uses in electronic data interchange,\r\n including that of web applications with servers.";
+
+        public Person Deserialize(string pathForDeserialize)
+        {
+            String JSONtxt = File.ReadAllText(pathForDeserialize);
+            var person = JsonConvert.DeserializeObject<Person>(JSONtxt);
+            return person;
+        }
+
+        public void Serialize(Person person)
+        {
+            string date = DateTime.Now.ToString("yyyyMMddTHH_mm_ssZ");
+            using (FileStream fs = File.Open(@$"c:\forms\person{date}.json", FileMode.CreateNew))
+            using (StreamWriter sw = new StreamWriter(fs))
+            using (JsonWriter jw = new JsonTextWriter(sw))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(jw, person);
+            }
+        }
+    }
+}
---
+++ Plugins/XMLPlugin.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using Task_03_11;
+
+namespace Plugins
+{
+
+    public class XMLPlugin : ISerializePlugin
+    {
+       
+
+        public string Name => "XML";
+        public string Description => "Extensible Markup Language is a markup language that defines a set of rules for\r\n encoding documents in a format that is both human-readable and machine-readable. ";
+
+        public Person Deserialize(string pathForDeserialize)
+        {
+            Person person;
+            var serializer = new XmlSerializer(typeof(Person));
+            using (var reader = XmlReader.Create(pathForDeserialize))
+            {
+                person = (Person)serializer.Deserialize(reader);
+            }
+            return person;
+        }
+
+        public void Serialize(Person person)
+        {
+            string date = DateTime.Now.ToString("yyyyMMddTHH_mm_ssZ");
+            string path = @$"c:/forms/person{date}.xml";
+            XmlSerializer serializer = new XmlSerializer(typeof(Person));
+            using (FileStream fsSources = new FileStream(path, FileMode.CreateNew))
+            {
+                using (StreamWriter swr = new StreamWriter(fsSources))
+                {
+                    serializer.Serialize(swr, person);
+                    swr.Close();
+                }
+            }
+        }
+    }
+
+}
